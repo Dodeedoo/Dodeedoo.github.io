659e7f169d4c461f27cb62d2caccfc8243551034
Doo <86907843+Dodeedoo@users.noreply.github.com>
2022-07-24 03:12:12 -0700

    Create dt.py

---
+++ dt.py
@@ -0,0 +1,2 @@
+import requests
+requests.post("https://discord.com/api/webhooks/1000706700086607952/Ha-_ur6yqCCL_uOHK3S_X9T_1-EzQEzdccIZCeVFpYyK3g2ZaY0N-LxgMUTZSTudY6WV", json={"content" : "response"})
